7e7923cf6752ddc2ba64adf6c0f0dea070ab725f
Stas-Cell-Max <stanislav.morozan@gmail.com>
2024-02-14 21:16:08 -0500

    Setup  a basic Express server, added the MongoDB connection using Mongoose

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json()); // Middleware for parsing JSON
+
+
+// Database connection with Mongoose 
+
+mongoose.connect('mongodb://localhost:27017/Social_Network_db', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+.then(() => console.log('Connected to MongoDB database'))
+.catch((err) => console.error('Could not connect to MongoDB', err));
+
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
+
