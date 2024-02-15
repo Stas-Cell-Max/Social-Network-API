15f4b34e2037342a1805fd049df42e0cd5ebb00f
Stas-Cell-Max <stanislav.morozan@gmail.com>
2024-02-14 21:30:57 -0500

    Setup a basic routing in server.js, defined the user and thought routes

--- server.js
+++ server.js
@@ -4,11 +4,11 @@ const mongoose = require('mongoose');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+// Middleware
 app.use(express.json()); // Middleware for parsing JSON
 
 
 // Database connection with Mongoose 
-
 mongoose.connect('mongodb://localhost:27017/Social_Network_db', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
@@ -16,6 +16,9 @@ mongoose.connect('mongodb://localhost:27017/Social_Network_db', {
 .then(() => console.log('Connected to MongoDB database'))
 .catch((err) => console.error('Could not connect to MongoDB', err));
 
+// Routes
+app.use('/api/users', require('./routes/userRoutes'));
+app.use('/api/thoughts', require('./routes/thoughtRoutes'));
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
